658d3096b36d66bfa23afda6afee116e252a57b1
celine bayliss <celinedubrule@gmail.com>
2026-06-07 19:45:56 -0400

    Fixed syntax error in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
+import React from 'react';
+import './index.css';
+
 function App() {
   return (
     <div className="App">
-      {/* Your app content goes here */}
       <h1>Hello, Simuscribe!</h1>
     </div>
   );
 }
 
-export default App; // <--- This line is critical!
-=======
-import { HashRouter, Routes, Route } from "react-router-dom";
-
-function App() {
-  return (
-    <HashRouter> 
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-      </Routes>
-    </HashRouter>
-  );
-}
->>>>>>> 1dc88e4fb43ddaa805d658f132ed21dbaae87f16
+export default App;

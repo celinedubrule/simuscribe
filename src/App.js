dd7b7a2fb8006ccf9467032d2b395b8d072fc742
celinedubrule <celinedubrule@gmail.com>
2026-06-07 17:31:03 -0400

    Refactor App component structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import "@/App.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import Landing from "@/pages/Landing";
-import Login from "@/pages/Login";
-import Dashboard from "@/pages/Dashboard";
-import ProtectedRoute from "@/components/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App">
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } />
-        </Routes>
-      </HashRouter>
-    </div>
+    <HashRouter> 
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+      </Routes>
+    </HashRouter>
   );
 }
-export default App;

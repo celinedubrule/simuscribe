edd13b3bb670c82a90f2b61a53b633c7ee15ff08
celine bayliss <celinedubrule@gmail.com>
2026-06-07 18:52:56 -0400

    Cleanup and force deploy

---
+++ README.md
@@ -0,0 +1,5 @@
+# SimuScribe
+The Master Curriculum Engine for accredited clinical simulation libraries.
+- Indexing 600+ cases.
+- SOAP framework integration.
+- Exportable for LMS compatibility.
---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      {/* Your app content goes here */}
+      <h1>Hello, Simuscribe!</h1>
+    </div>
+  );
+}
+
+export default App; // <--- This line is critical!
